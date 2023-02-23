74469ec88b271fdd1a41e936cdb67b24d1940c5e
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-23 15:26:42 -0600

    1526-applying colors

--- src/main/java/main/ContactManager.java
+++ src/main/java/main/ContactManager.java
@@ -1,30 +1,41 @@
 package main;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Scanner;
+        import java.io.File;
+        import java.io.FileWriter;
+        import java.io.IOException;
+        import java.nio.file.Path;
+        import java.nio.file.Paths;
+        import java.util.ArrayList;
+        import java.util.Scanner;
 
 public class ContactManager {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public static void draw(){
-        System.out.println("""
+        System.out.println(ANSI_GREEN +
+                """
                 ______ _                       ______             _   \s
                 | ___ | |                      | ___ \\           | |  \s
                 | |_/ | |__   ___  _ __   ___  | |_/ / ___   ___ | | __
                 |  __/| '_ \\ / _ \\| '_ \\ / _ \\ | ___ \\/ _ \\ / _ \\| |/ /
                 | |   | | | | (_) | | | |  __/ | |_/ | (_) | (_) |   <\s
                 \\_|   |_| |_|\\___/|_| |_|\\___| \\____/ \\___/ \\___/|_|\\_\\
-                """);
+                """ + ANSI_RESET);
     }
     public static void showMainMenu() {
         System.out.println("1. View contacts.");
-        System.out.println("2. Add a new contact.");
+        System.out.println(ANSI_BLUE + "2. Add a new contact." + ANSI_RESET);
         System.out.println("3. Search a contact by name.");
-        System.out.println("4. Delete an existing contact.");
+        System.out.println(ANSI_RED + "4. Delete an existing contact." + ANSI_RESET);
         System.out.println("5. Exit.");
         System.out.print("Enter an option (1, 2, 3, 4 or 5): ");
     }
@@ -52,12 +63,12 @@ public class ContactManager {
 
     public static void addContact(ArrayList<Contact> contacts) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter name: ");
+        System.out.print(ANSI_BLUE + "Enter name: " + ANSI_RESET);
         String name = scanner.nextLine();
-        System.out.print("Enter phone number: ");
+        System.out.print(ANSI_BLUE + "Enter phone number: " + ANSI_RESET);
         String phoneNumber = scanner.nextLine();
         String number = phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3");
-        System.out.println("Contact added.");
+        System.out.println(ANSI_BLUE + "Contact added." + ANSI_RESET);
         Contact aNewContact = Contact.add(new Contact(capitalizeWords(name), number));
         contacts.add(aNewContact);
         printContacts(contacts);
@@ -82,19 +93,24 @@ public class ContactManager {
                 System.out.printf("---------------------------------\n");
                 System.out.printf("| %-15s| %-13s|\n", contact.contactName, contact.contactNumber);
                 System.out.printf("---------------------------------\n");
+
             }
         }
     }
 
     public static void deleteContact(ArrayList<Contact> contacts) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the contact name to delete: ");
+        System.out.print(ANSI_RED + "Enter the contact name to delete: " + ANSI_RESET);
         String name = scanner.nextLine();
 
         for (int i = contacts.size() -1; i >= 0; i--) {
             if (contacts.get(i).contactName.contains(capitalizeWords(name))) {
-                System.out.println("deleting: " + contacts.get(i).contactName);
-                contacts.remove(contacts.get(i));
+                System.out.println(ANSI_RED + "Would you like to delete " + contacts.get(i).contactName + "(yes/no) :" + ANSI_RESET);
+                String deleteChoice = scanner.nextLine();
+                if (deleteChoice.equalsIgnoreCase("yes")){
+                    System.out.println(ANSI_RED + "Deleting: " + contacts.get(i).contactName + ANSI_RESET);
+                    contacts.remove(contacts.get(i));
+                }
             }
         }
         printContacts(contacts);
